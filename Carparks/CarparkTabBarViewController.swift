b0d22cc37db739133cdfc7a847a22a05482f085d
Eddie Lee <eddielee6@me.com>
2016-05-30 18:24:33 +0100

    Request users location

--- Carparks/CarparkTabBarViewController.swift
+++ Carparks/CarparkTabBarViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CarparkTabBarViewController: UITabBarController {
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        requestUserLocation()
+    }
     
     override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
         if let selectedNavigationController = viewControllers?[selectedIndex] as? UINavigationController {
             selectedNavigationController.popToRootViewControllerAnimated(false)
         }
+    }
+}
+
+extension CarparkTabBarViewController: CLLocationManagerDelegate {
+    private func requestUserLocation() {
+        guard CLLocationManager.locationServicesEnabled() else {
+            print("Location Services not available")
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 }
